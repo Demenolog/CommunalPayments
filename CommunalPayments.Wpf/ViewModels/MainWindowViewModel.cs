00689498c3b0a2f076251ed664157f2249e94b58
Denis Nikolenko <Demenolog0@yandex.ru>
2023-06-30 21:54:12 +0500

    Создание и привязка свойств для блока "Общая информация"

--- CommunalPayments.Wpf/ViewModels/MainWindowViewModel.cs
+++ CommunalPayments.Wpf/ViewModels/MainWindowViewModel.cs
@@ -23,5 +23,49 @@ namespace CommunalPayments.Wpf.ViewModels
         #endregion Свойства
 
         #endregion Главное окно
+
+        #region Блок - Общая информация
+
+        #region Свойства
+
+        #region CalculationYear : string - Выбранный год для расчёта
+
+        private string _calculationYear;
+
+        public string CalculationYear
+        {
+            get => _calculationYear;
+            set => SetField(ref _calculationYear, value);
+        }
+
+        #endregion CalculationYear : string - Выбранный год для расчёта
+
+        #region CalculationMonth : string - Выбранный месяц для расчёта
+
+        private string _calculationMonth;
+
+        public string CalculationMonth
+        {
+            get => _calculationMonth;
+            set => SetField(ref _calculationMonth, value);
+        }
+
+        #endregion CalculationMonth : string - Выбранный месяц для расчёта
+
+        #region NumberResidents : string - Количество проживающих на момент расчёта
+
+        private string _numberResidents;
+
+        public string NumberResidents
+        {
+            get => _numberResidents;
+            set => SetField(ref _numberResidents, value);
+        }
+
+        #endregion NumberResidents : string - Количество проживающих на момент расчёта
+       
+        #endregion
+
+        #endregion Блок - Общая информация
     }
 }
