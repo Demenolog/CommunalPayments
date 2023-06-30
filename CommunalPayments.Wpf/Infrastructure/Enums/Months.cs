d50cb3619cdd3a7ec8eed27c29acd476281ddad7
Denis Nikolenko <Demenolog0@yandex.ru>
2023-06-30 21:26:23 +0500

    Создание службы для вывода месяцов в ComboBox

---
+++ CommunalPayments.Wpf/Infrastructure/Enums/Months.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPayments.Wpf.Infrastructure.Enums
+{
+    public enum Months
+    {
+        Январь,
+        Февраль,
+        Март,
+        Апрель,
+        Май,
+        Июнь,
+        Июль,
+        Август,
+        Сентябрь,
+        Октябрь,
+        Ноябрь,
+        Декабрь
+    }
+}
---
+++ CommunalPayments.Wpf/Services/EnumBindingSourceExtensionService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace CommunalPayments.Wpf.Services
+{
+    public class EnumBindingSourceExtensionService : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumBindingSourceExtensionService(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+            {
+                throw new ArgumentException("EnumType must not be null and of type Enum");
+            }
+
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
