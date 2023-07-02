e6a83133d6ddc5f78e772137cbc4f3f65c96a27b
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-02 12:14:10 +0500

    Тестовая команда для подключения к БД

---
+++ CommunalPayments.Common.DataContext.Sqlite/ReceiptContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace CommunalPayments.Common.DataContext.Sqlite
+{
+    public static class ReceiptDb
+    {
+        private static readonly string ConnectionString;
+
+        static ReceiptDb()
+        {
+            ConnectionString = $@"Data Source={Environment.CurrentDirectory}mydb.db;Version=3;";
+        }
+
+        public static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection connection = new SQLiteConnection(ConnectionString);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine(nameof(ex));
+                throw;
+            }
+
+            return connection;
+        }
+    }
+}
