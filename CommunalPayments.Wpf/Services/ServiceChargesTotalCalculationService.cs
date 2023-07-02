459beb111708b3adb7a6692ce27f4a5dc02f8b0f
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-02 15:13:39 +0500

    Создание сервиса для подсчёта общей суммы начислений

---
+++ CommunalPayments.Wpf/Services/ServiceChargesTotalCalculationService.cs
@@ -0,0 +1,26 @@
+﻿using CommunalPayments.Wpf.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPayments.Wpf.Services
+{
+    public static class ServiceChargesTotalCalculationService
+    {
+        private static readonly MainWindowViewModel MainWindow = new ViewModelLocator().MainWindowModel;
+
+        public static void Calculate()
+        {
+            var serviceChargesCold = decimal.Parse(MainWindow.ServiceChargesCold);
+            var serviceChargesHotHeatCarrier = decimal.Parse(MainWindow.ServiceChargesHotHeatCarrier);
+            var serviceChargesHotHeatEnergy = decimal.Parse(MainWindow.ServiceChargesHotHeatEnergy);
+            var serviceChargesEnergy = decimal.Parse(MainWindow.ServiceChargesEnergy);
+
+            var serviceChargesTotal = serviceChargesCold + serviceChargesHotHeatCarrier + serviceChargesHotHeatEnergy + serviceChargesEnergy;
+
+            MainWindow.ServiceChargesTotal = serviceChargesTotal.ToString();
+        }
+    }
+}
