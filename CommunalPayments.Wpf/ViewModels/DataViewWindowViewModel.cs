e2134210e494058a81da04835be811b83f3d5ec6
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 13:19:33 +0500

    Создание и привязка команды Привязка ресурсов к dataGrid, настройка колонок datagrid

--- CommunalPayments.Wpf/ViewModels/DataViewWindowViewModel.cs
+++ CommunalPayments.Wpf/ViewModels/DataViewWindowViewModel.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunalPayments.Common.DataContext.Sqlite.Models;
+using CommunalPayments.Wpf.Infrastructure.Commands;
 using CommunalPayments.Wpf.ViewModels.Base;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Windows.Input;
+using CommunalPayments.Wpf.Services;
 
 namespace CommunalPayments.Wpf.ViewModels
 {
     internal class DataViewWindowViewModel : ViewModel
     {
+        #region Свойства
+
         #region Title : string - Заголовок окна
 
         private string _title = "Прошлые показания";
@@ -20,5 +23,37 @@ namespace CommunalPayments.Wpf.ViewModels
         }
 
         #endregion Title : string - Заголовок окна
+
+        #region Receipts : ObservableCollection<ReceiptData> - Данные из БД
+
+        private ObservableCollection<ReceiptData> _receipts;
+
+        public ObservableCollection<ReceiptData> Receipts
+        {
+            get => _receipts;
+            set => SetField(ref _receipts, value);
+        }
+
+        #endregion Receipts : ObservableCollection<ReceiptData> - Данные из БД
+
+        #region UpdateDataGrid command
+
+        public ICommand UpdateDataGrid { get; }
+
+        private bool CanUpdateDataGridExecuted(object p) => true;
+
+        private void OnUpdateDataGridExecute(object p)
+        {
+            DatabaseControlService.UpdateDataGrid();
+        }
+
+        #endregion UpdateDataGrid command
+
+        #endregion Свойства
+
+        public DataViewWindowViewModel()
+        {
+            UpdateDataGrid = new LambdaCommand(OnUpdateDataGridExecute, CanUpdateDataGridExecuted);
+        }
     }
 }
