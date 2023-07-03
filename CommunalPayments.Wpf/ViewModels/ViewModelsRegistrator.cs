ce988e3cc7943e081135e9d5d3f2b44a1b242934
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 10:13:15 +0500

    Регистрация dataViewViewModel

--- CommunalPayments.Wpf/ViewModels/ViewModelsRegistrator.cs
+++ CommunalPayments.Wpf/ViewModels/ViewModelsRegistrator.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommunalPayments.Wpf.ViewModels
 {
     internal static class ViewModelsRegistrator
     {
         public static IServiceCollection AddViewModel(this IServiceCollection services) => services
-            .AddSingleton<MainWindowViewModel>();
+            .AddSingleton<MainWindowViewModel>()
+            .AddSingleton<DataViewWindowViewModel>();
     }
 }
