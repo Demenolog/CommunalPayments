328f1e671baf269e72a2fddfebf4283af9324c3b
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 10:07:53 +0500

    Добавление сервиса для управления окном с данными из БД

---
+++ CommunalPayments.Wpf/Services/DataViewWindowService.cs
@@ -0,0 +1,40 @@
+﻿using CommunalPayments.Wpf.Views;
+
+namespace CommunalPayments.Wpf.Services
+{
+    internal static class DataViewWindowService
+    {
+        private static DataViewWindow s_dataViewWindow = null;
+
+        public static bool IsCreated => s_dataViewWindow != null;
+
+        public static DataViewWindow DataViewWindow
+        {
+            get => s_dataViewWindow;
+            set => s_dataViewWindow = value;
+        }
+
+        public static void Create()
+        {
+            if (DataViewWindow != null) return;
+
+            DataViewWindow = new DataViewWindow();
+            DataViewWindow.Closed += (o, args) => DataViewWindow = null;
+            //DataViewWindow.Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/Icons/Secondary/Gear.ico"));
+
+            ChildWindowService.Add(DataViewWindow);
+        }
+
+        public static bool Show()
+        {
+            if (DataViewWindow != null)
+            {
+                DataViewWindow.Show();
+                DataViewWindow.Focus();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
