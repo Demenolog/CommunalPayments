4a8bd77b32fc510716954dad56493ff70cd23c00
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 10:30:09 +0500

    Добавление и привязка свойства заголовка

--- CommunalPayments.Wpf/ViewModels/DataViewWindowViewModel.cs
+++ CommunalPayments.Wpf/ViewModels/DataViewWindowViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunalPayments.Wpf.ViewModels.Base;
 
 namespace CommunalPayments.Wpf.ViewModels
 {
-    internal class DataViewWindowViewModel
+    internal class DataViewWindowViewModel : ViewModel
     {
+        #region Title : string - Заголовок окна
+
+        private string _title = "Прошлые показания";
+
+        public string Title
+        {
+            get => _title;
+            set => SetField(ref _title, value);
+        }
+
+        #endregion Title : string - Заголовок окна
     }
 }
