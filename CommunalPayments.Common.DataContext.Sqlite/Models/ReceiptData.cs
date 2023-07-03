004ad960723edf2eecf1da83d399748ffb9383ac
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 12:23:32 +0500

    Добавление модели базы данных

---
+++ CommunalPayments.Common.DataContext.Sqlite/Models/ReceiptData.cs
@@ -0,0 +1,33 @@
+﻿namespace CommunalPayments.Common.DataContext.Sqlite.Models
+{
+    public class ReceiptData
+    {
+        public int Id { get; set; }
+
+        public string CalculationYear { get; set; }
+
+        public string CalculationMonth { get; set; }
+
+        public string ConsumptionValueCold { get; set; }
+
+        public string ServiceChargesCold { get; set; }
+
+        public string ConsumptionValueHotHeatCarrier { get; set; }
+
+        public string ConsumptionValueHotHeatEnergy { get; set; }
+
+        public string ServiceChargesHotHeatCarrier { get; set; }
+
+        public string ServiceChargesHotHeatEnergy { get; set; }
+
+        public string ConsumptionValueEnergyDay { get; set; }
+
+        public string ConsumptionValueEnergyNight { get; set; }
+
+        public string ConsumptionValueEnergyGeneral { get; set; }
+
+        public string ServiceChargesEnergy { get; set; }
+
+        public string ServiceChargesTotal { get; set; }
+    }
+}
