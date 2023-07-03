06245b79dfdc9820280790c38e380282be1fab93
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 19:17:18 +0500

    Изменение формата строк

--- CommunalPayments.Wpf/Infrastructure/Constans/CalculatedValuesFormatConstans.cs
+++ CommunalPayments.Wpf/Infrastructure/Constans/CalculatedValuesFormatConstans.cs
@@ -8,8 +8,8 @@ namespace CommunalPayments.Wpf.Infrastructure.Constans
 {
     internal static class CalculatedValuesFormatConstans
     {
-        public const string Common = "0,0.#####";
+        public const string Common = "0.#####";
 
-        public const string Money = "0,0.00";
+        public const string Money = "0.00";
     }
 }
