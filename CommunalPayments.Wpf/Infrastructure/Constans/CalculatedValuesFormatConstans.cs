132bd74745484e0c1de52315cf0da226a52c1fcd
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 15:49:51 +0500

    Создание класса с форматом строк для расчитанных значений

---
+++ CommunalPayments.Wpf/Infrastructure/Constans/CalculatedValuesFormatConstans.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPayments.Wpf.Infrastructure.Constans
+{
+    internal static class CalculatedValuesFormatConstans
+    {
+        public const string Common = "0,0.#####";
+
+        public const string Money = "0,0.00";
+    }
+}
