e2db6dc0b1923fa6c6c0a6489da6ccc8f10e4629
Denis Nikolenko <Demenolog0@yandex.ru>
2023-07-03 21:02:04 +0500

    Создание класса для оповещения юзера через messageBox

---
+++ CommunalPayments.Wpf/Services/UserAlertService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CommunalPayments.Wpf.Services
+{
+    internal static class UserAlertService
+    {
+        public static void Information(string information)
+        {
+            MessageBox.Show(information, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void Error(string error)
+        {
+            MessageBox.Show(error, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
